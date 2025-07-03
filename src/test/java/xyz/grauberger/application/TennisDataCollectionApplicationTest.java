c064fe594f132d5f90a679e4e69c6ca3d215ab71
Rudolf Grauberger <rudolf.grauberger@impect.com>
2025-07-03 18:49:11 +0200

    Add Spring Modulith
    Update test class for application structure verification and application module documentation generation.

--- src/test/java/xyz/grauberger/application/TennisDataCollectionApplicationTest.java
+++ src/test/java/xyz/grauberger/application/TennisDataCollectionApplicationTest.java
@@ -5,7 +5,10 @@ import xyz.grauberger.application.collection.CollectionTestDataTestConfiguration
 import xyz.grauberger.application.fdi.FdiTestDataTestConfiguration;
 import xyz.grauberger.application.masterdata.MasterdataTestDataConfiguration;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SuppressWarnings("java:S2187") // This class is used for local development only
 public class TennisDataCollectionApplicationTest {
@@ -18,5 +21,20 @@ public class TennisDataCollectionApplicationTest {
                 .with(IdMappingTestDataTestConfiguration.class)
                 .with(CollectionTestDataTestConfiguration.class)
                 .run(args);
+    }
+
+    @Test
+    void verifyApplicationStructure() {
+        ApplicationModules.of(TennisDataCollectionApplication.class)
+            .verify();
+    }
+
+    @Test
+    @SuppressWarnings("java:S2699")
+    void writeDocumentation() {
+        var modules = ApplicationModules.of(TennisDataCollectionApplication.class);
+
+        new Documenter(modules)
+                .writeDocumentation();
     }
 }
